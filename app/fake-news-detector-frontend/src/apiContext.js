2c82acc1e6a51c82a4139a89b59efc343059c903
rahul122703 <killbusyness@gmail.com>
2025-03-30 16:01:50 +0530

    modifed requirements.txt added env for apicontext

--- app/fake-news-detector-frontend/src/apiContext.js
+++ app/fake-news-detector-frontend/src/apiContext.js
@@ -3,7 +3,9 @@ import React, { createContext, useContext } from "react";
 const ApiContext = createContext();
 
 const ApiProvider = ({ children }) => {
-  const api_link = "http://127.0.0.1:8000";
+  const api_link = process.env.REACT_APP_BACKEND_API_URL;
+
+  console.log("------------->>>>>>>>>API URL:", api_link);
 
   return (
     <ApiContext.Provider value={{ api_link }}>{children}</ApiContext.Provider>
