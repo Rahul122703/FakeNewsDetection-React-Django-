84ef30eb2a0aac2820b00c6b4a414ce2915f5b0d
rahul122703 <killbusyness@gmail.com>
2025-03-30 17:01:07 +0530

    remoed loader js from checkbytitle.js

--- app/fake-news-detector-frontend/src/components/checkbytitle.js
+++ app/fake-news-detector-frontend/src/components/checkbytitle.js
@@ -4,7 +4,6 @@ import "react-toastify/dist/ReactToastify.css";
 import Header from "./header";
 import Axios from "axios";
 import { useApiContext } from "../apiContext";
-import Loader from "./loader";
 
 function CheckByTitle() {
   document.title = "Check news by title";
@@ -13,7 +12,6 @@ function CheckByTitle() {
   const [predictedValue, setPredictedValue] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const { api_link } = useApiContext();
-
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
@@ -66,11 +64,10 @@ function CheckByTitle() {
           <button
             type="submit"
             className="w-full bg-blue-500 text-white font-medium py-2 rounded-lg hover:bg-blue-600 transition-all">
-            {isLoading ? <Loader /> : "Check"}
+            {isLoading ? "Checking..." : "Check"}
           </button>
         </form>
-        {isLoading && <Loader />}
-        {!isLoading && predictedValue && (
+        {predictedValue && (
           <div
             className={`text-lg font-semibold text-center py-2 rounded-lg ${
               predictedValue === "True"
